1fe1818f439e5b3bdf804104a929380a931030e5
Dianminii <dianmiftahul02@gmail.com>
2020-11-11 12:59:34 +0700

    UTS OOP

---
+++ Persegi/Persegi.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persegi;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Persegi {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+        int sisi;
+        int hasilkeliling;
+        int hasilluas;
+        
+        sisi = 36;
+        
+        hasilkeliling = sisi*4;
+        hasilluas = sisi * sisi;
+        
+        System.out.println ("Hasil Keliling Persegi :"+ hasilkeliling);
+        System.out.println ("Hasil Luas Persegi"+ hasilluas);
+    }
+}
